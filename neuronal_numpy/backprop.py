17e6d1c058f42d4ef3e8aac6adaa21b8f2a44ce1
fcoverdugoa <fcoverdugoa@underpost.net>
2021-04-30 17:09:55 -0400

    update numpy nt

--- neuronal_numpy/backprop.py
+++ neuronal_numpy/backprop.py
@@ -1,7 +1,14 @@
 import numpy as np
 
+# https://www.youtube.com/watch?v=jKPWYlOxPMU
+
 from data_prep import features,targets, features_test, targets_test
 
+print('--------------')
+print('Init BackoProp')
+print('--------------')
+
+# funcion establecida por una regresion lineal ?
 def sigmoide(x):
     return 1/(1 + np.exp(-x))
 
@@ -11,17 +18,31 @@ n_hidden = 2 # Número de unidades en la capa escondida
 epochs = 1000 # Número de iteraciones sobre el conjunto de entrenamiento
 alpha = 0.01 # Taza de aprendizaje
 
-ult_costo = None 
+ult_costo = None
 
-m,k = features.shape # Número de ejemplos de entrenamiento, número de dimensiones en los datos 
+m,k = features.shape # Número de ejemplos de entrenamiento, número de dimensiones en los datos
+
+print('(m) numero ejemplos entrenamiento ->')
+print(m)
+print('(k) numero de dimensiones datos ->')
+print(k)
 
 # Inicialización de los pesos
-entrada_escondida = np.random.normal(scale = 1/k**0.5,
-                                     size = (k,n_hidden)
-                                     )
-escondida_salida = np.random.normal(scale = 1/k**0.5,
-                                    size = n_hidden
-                                    )
+# Scale-> Standard deviation
+scale = 1/k**0.5
+# scale = 0
+print('(scale) (1/k**0.5) ->')
+print(scale)
+
+# np.random.normal(mu, sigma, size_data_number)
+
+print('(entrada_escondida) ->')
+entrada_escondida = np.random.normal(0, scale, size = (k,n_hidden) )
+print(entrada_escondida)
+
+print('(escondida_salida) ->')
+escondida_salida = np.random.normal(0, scale, size = n_hidden )
+print(escondida_salida)
 
 # Entrenamiento
 
@@ -31,12 +52,56 @@ for e in range(epochs):
     gradiente_entrada_escondida = np.zeros(entrada_escondida.shape)
     gradiente_escondida_salida =  np.zeros(escondida_salida.shape)
 
-    # Itera sobre el conjunto de entrenamiento
+    # print('--------------------------------')
+    # print('epochs ->')
+    # print(e)
+    # print('gradiente_entrada_escondida ->')
+    # print(gradiente_entrada_escondida)
+    # print('gradiente_escondida_salida ->')
+    # print(gradiente_escondida_salida)
 
-    for x,y in zip(features.values,targets):
+
+    # Itera sobre el conjunto de entrenamiento
+    _zip = zip(features.values,targets);
+
+    # print('---')
+    # print(type(features.values[0]))
+    # print(type(targets))
+
+    # el zip lo que hace es unir 2 listas para luego ser iteradas de una forma previamente definida
+
+    # print('--->')
+    # print(_zip);
+    # print(tuple(_zip));
+    # (array([ 0.10647826, -0.68292878,  0.        ,  1.        ,  0.        ,  0.        ]), 0)
+
+    for x,y in _zip:
+
+        # print('x ->')
+        # print(x)
+        # print('y ->')
+        # print(y)
+
         # Pasada hacia adelande (forward pass)
-        z = sigmoide(np.matmul(x, entrada_escondida))
-        y_ =sigmoide(np.matmul(escondida_salida,z)) # predicción 
+
+        # producto punto o escala -> un numero
+        # producto matricial -> matriz
+
+        # matmul -> producto matricial
+
+        z_matmul = np.matmul(x, entrada_escondida);
+        # print('(z_matmul) np.matmul(x, entrada_escondida) ->')
+        # print(z_matmul)
+
+        z = sigmoide(z_matmul)
+        # print('(z) sigmoide ->')
+        # print(z)
+
+        # print('---')
+        # print(type(escondida_salida))
+        # print(type(z))
+
+        y_ = sigmoide(np.matmul(escondida_salida,z)) # predicción
 
         # Pasada hacia atrás (backward pass)
         salida_error = (y - y_) * y_ *(1- y_)
@@ -44,12 +109,12 @@ for e in range(epochs):
         escondida_error = np.dot(salida_error, escondida_salida) * z * (1 -z)
 
         gradiente_entrada_escondida += escondida_error * x[:,None]
-        gradiente_escondida_salida += salida_error * z 
+        gradiente_escondida_salida += salida_error * z
 
 
     # Actualiza los parámetros (pesos)
-    entrada_escondida += alpha * gradiente_entrada_escondida / m 
-    escondida_salida +=  alpha * gradiente_escondida_salida / m 
+    entrada_escondida += alpha * gradiente_entrada_escondida / m
+    escondida_salida +=  alpha * gradiente_escondida_salida / m
 
     if e % (epochs / 10 ) == 0:
         z = sigmoide(np.dot(features.values, entrada_escondida))
@@ -62,30 +127,32 @@ for e in range(epochs):
             print("Costo de  entrenamiento: ", costo, " ADVERTENCIA -  Costo subiendo")
         else:
             print("Costo de entrenamiento: ", costo )
-        
-        ult_costo = costo 
 
-#  Precisión en los datos de prueba 
+        ult_costo = costo
+
+#  Precisión en los datos de prueba
+
+print('features_test ->')
+print(features_test)
+
 z = sigmoide(np.dot(features_test, entrada_escondida))
+
+print('(z final) ->')
+print(z)
+
 y_ = sigmoide(np.dot(z, escondida_salida))
 
-predicciones =  y_ > 0.5 
+print('(y_ final) ->')
+print(y_)
+
+predicciones =  y_ > 0.5
+
+print('predicciones ->')
+print(predicciones)
+
 precision = np.mean(predicciones == targets_test)
+
+print('precision ->')
+print(precision)
+
 print("Precisión: {:.3f}".format(precision))
-
-
-
-
-
-
-
-
-
-
-
-        
-
-
-
-
-
