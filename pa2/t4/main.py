6128213915db2791fd35c8a48f64b4481f91320e
fcoverdugo <fcoverdugoa@underpost.net>
2022-04-25 20:19:18 -0400

    update

--- pa2/t4/main.py
+++ pa2/t4/main.py
@@ -1,23 +1,22 @@
 
-
-
-
-
-
 import tkinter
-
 
 form = tkinter.Tk()
 form.geometry("500x400")
 
-def defcompose():
-    result = "test"
-    result = tkinter.Label(form, text= result)
-    result.pack()
-
 inputNumber = tkinter.Entry(form)
 inputNumber.pack()
 
+def defcompose():
+    result = ""
+    for n in range(0, len(inputNumber.get())):
+        op = int(inputNumber.get()) // 10**n % 10;
+        result += str(op) + ("0"*n) + "\n"
+
+    result = tkinter.Label(form, text= result)
+    result.pack()
+
+print()
 
 btn = tkinter.Button(form, text="Descomponer", command = defcompose)
 btn.pack()
