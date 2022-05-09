4491812eec308f88192834560f196a6cb8827b98
fcoverdugo <fcoverdugoa@underpost.net>
2022-05-09 11:18:00 -0400

    update

--- pa2/t6/t2.py
+++ pa2/t6/t2.py
@@ -7,9 +7,29 @@ class MiClase:
         self.lista = items
     def __iter__(self):
         return iter(self.lista)
+    def __test__(self):
+        print('test')
 
 
-        
+
 miobjeto = MiClase([5, 4, 3])
 for item in miobjeto:
     print(item)
+
+
+
+libro = ['página1', 'página2', 'página3', 'página4']
+marcapaginas = iter(libro)
+
+print(next(marcapaginas))
+print(next(marcapaginas))
+print(next(marcapaginas))
+print(next(marcapaginas))
+
+print(libro.copy())
+print(miobjeto.__test__())
+
+a = 'a'
+b = a
+b = 'b'
+print(a)
